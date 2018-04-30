d1c06c3f72d6743ea6162ddeefa309294026ae86
AndreaSibilia <a.sibilia89@gmail.com>
2018-04-30 09:34:38 -0400

    Uploading a template with a background map (leaflet) and the track location functionality

---
+++ ucesibi/www/js/trackLocation.js
@@ -0,0 +1,14 @@
+function trackLocation(){
+	if(navigator.geolocation){
+		navigator.geolocation.watchPosition(showPosition);
+	} else {
+		document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
+function showPosition(position){
+	document.getElementById('showLocation').innerHTML = "Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude;
+	var x = position.coords.latitude;
+	var y = position.coords.longitude;
+	L.marker([x,y]).addTo(mymap).bindPopup("This is your location").openPopup();
+	return [x,y]
+}
